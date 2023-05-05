0f5c68de1fcdd2ee96fc38140b34b80f80348d18
Joe Zane <joezane.cn@gmail.com>
2023-05-05 19:30:17 +0800

    1. update code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use winit::{
+    dpi::{PhysicalSize, Size},
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    platform::windows::WindowExtWindows,
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+
+    let window = WindowBuilder::new()
+        .with_title("Winit")
+        .with_inner_size(Size::Physical(PhysicalSize::new(1280, 800)))
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+        // dispatched any events. This is ideal for games and similar applications.
+        control_flow.set_poll();
+
+        // ControlFlow::Wait pauses the event loop if no events are available to process.
+        // This is ideal for non-game applications that only update in response to user
+        // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+        control_flow.set_wait();
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                control_flow.set_exit();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::Moved(pos),
+                ..
+            } => {
+                println!("Window has moved, position: {:?}", pos)
+            }
+            Event::MainEventsCleared => {
+                // Application update code.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw, in
+                // applications which do not always need to. Applications that redraw continuously
+                // can just render here instead.
+                window.request_redraw();
+            }
+            Event::RedrawRequested(_) => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in MainEventsCleared, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+                let _hwnd = window.hwnd();
+                
+            }
+            _ => (),
+        }
+    });
 }
